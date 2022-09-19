99aa03cfcfd5672c18421798bdd40167894e0083
Fanny-Lis <f.botis.s849@edu.utc.mx>
2022-09-19 10:55:47 -0500

    porque a mi

---
+++ 02 hola mundo/HolaMundo.java
@@ -0,0 +1,19 @@
+//comentario
+/*
+*este comentario nos sirve para poder tener varias lineas 
+comentadas del código
+*Todo programa en Java maneja clases, 
+una clase es un objeto del mundo real
+*/
+class holamundo{
+//cuerpo del programa
+/* 
+*aqui es donde nosotros codificamos todos los elemntos del programa
+,funciones, estructuras, variables y metodos 
+*toda clase debe de tener un metodo principal
+*/
+
+public static void main(String[]args){
+    System.out.println("Hola Mundo");
+}
+}
